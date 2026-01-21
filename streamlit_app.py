a76e65ec7d5c0f41825e5e157f9013ad5bdd2a28
Mariana <18ramirezmariana@gmail.com>
2026-01-21 11:14:52 -0500

    password added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os,importlib
 from assets.sidebar import sidebar_style
+from assets.auth import check_login
 from assets.helper_funcs import initialize_vars
 st.set_page_config(page_title=" Padel App",page_icon=":tennis:", layout="wide")
 
@@ -12,6 +13,8 @@ hide_streamlit_style = """
     </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+if not check_login():
+    st.stop()
 
 # Cargar la lista de páginas desde la carpeta "pages"
 pages_list = ["home"] + [f.replace(".py", "") for f in os.listdir("pages") if f.endswith(".py")]
